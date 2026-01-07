8fbd60b400fe51aa358da6619ef1b6d7abec9ed0
Evgeny Kislov <dev@evgenykislov.com>
2026-01-07 23:28:42 +0300

    Добавлена отсылка сообщений о соединении и с данными.

--- common/trunklink.cpp
+++ common/trunklink.cpp
@@ -2,25 +2,18 @@
 
 #include <iostream>
 
-enum Command : uint32_t {
-  kCommandCreateConnect = 1,
-  kCommandReleaseConnect = 2,
-  kCommandAckCreateConnect = 3,
-  kCommandDataOut = 11,
-  kCommandDataIn = 12,
-  kCommandAckDataOut = 21,
-  kCommandAckDataIn = 22
-};
 
 const size_t kConnectIDSize = 16;
+const unsigned int kTimeout = 300;
 
 struct PacketHeader {
-  Command PacketCommand;
   uint8_t ConnectID[kConnectIDSize];
+  TrunkCommand PacketCommand;
 };
 
 struct PacketConnect: PacketHeader {
   uint32_t PointID;
+  uint32_t Timeout;
 };
 
 
@@ -37,7 +30,7 @@ struct PacketAck: PacketHeader {
 
 TrunkClient::TrunkClient(boost::asio::io_context& ctx,
     const std::vector<boost::asio::ip::udp::endpoint>& trpoints)
-    : points_(trpoints) {
+    : points_(trpoints), trunk_socket_(ctx, boost::asio::ip::udp::v4()) {
   // Инициализация генератора uuid
   std::random_device rd;
   auto seed_data = std::array<int, std::mt19937::state_size>{};
@@ -59,11 +52,16 @@ ConnectID TrunkClient::CreateConnect(PointID point,
   ci.ID = id;
   ci.OnDisconnect = on_disconnect;
   ci.OnData = on_data;
+  ci.NextIndex = 0;
+  // ci.Status = // TODO Remove status ???
 
-  std::unique_lock<std::mutex> lk(connect_lock_);
+  std::unique_lock<std::mutex> lk(connects_lock_);
   connects_.push_back(ci);
   lk.unlock();
 
+  SendConnect(id, point, kTimeout);
+
+  //  trunk_socket_.async_send_to()
 
   return id;
 }
@@ -71,7 +69,7 @@ ConnectID TrunkClient::CreateConnect(PointID point,
 void TrunkClient::ReleaseConnect(ConnectID cnt) noexcept {
   ConnectInfo ci;
   bool find = false;
-  std::unique_lock<std::mutex> lk(connect_lock_);
+  std::unique_lock<std::mutex> lk(connects_lock_);
   for (auto i = connects_.begin(); i != connects_.end(); ++i) {
     if (i->ID == cnt) {
       ci = *i;
@@ -90,8 +88,77 @@ void TrunkClient::ReleaseConnect(ConnectID cnt) noexcept {
   ci.OnDisconnect(ci.ID);
 }
 
+void TrunkClient::SendConnect(
+    ConnectID cnt, PointID point, unsigned int timeout) {
+  auto buf = GetBuffer();
+  PacketConnectCache pc;
+  pc.PacketData = buf;
+  pc.PacketSize = sizeof(PacketConnect);
+  pc.CtxID = cnt;
+
+  assert(sizeof(PacketConnect) <= kPacketBufferSize);
+  auto pkt = (PacketConnect*)(pc.PacketData.get());
+  auto cnt_bin = cnt.as_bytes();
+  assert(cnt_bin.size_bytes() == kConnectIDSize);
+  memcpy(pkt->ConnectID, cnt_bin.data(), kConnectIDSize);
+  pkt->PacketCommand = kTrunkCommandCreateConnect;
+  pkt->PointID = point;
+  pkt->Timeout = timeout;
+
+  std::unique_lock<std::mutex> lk(packet_cache_lock_);
+  packet_connect_cache_.push_back(pc);
+  lk.unlock();
+
+  trunk_socket_.async_send_to(boost::asio::buffer(buf.get(), pc.PacketSize),
+      points_.front(),
+      [buf](boost::system::error_code /*ec*/, std::size_t /*bytes_sent*/) {});
+}
+
+std::shared_ptr<TrunkClient::PacketBuffer> TrunkClient::GetBuffer() {
+  return std::make_shared<TrunkClient::PacketBuffer>();
+}
+
+uint32_t TrunkClient::GetPacketIndex(ConnectID cnt) {
+  std::lock_guard<std::mutex> lk(connects_lock_);
+  for (auto& item : connects_) {
+    if (item.ID == cnt) {
+      auto res = item.NextIndex;
+      ++item.NextIndex;
+      return res;
+    }
+  }
+  return kBadPacketIndex;
+}
+
 bool TrunkClient::SendData(ConnectID cnt, void* data, size_t data_size) {
-  std::printf("DEBUG: send %u bytes to connect %s\n", data_size,
+  std::printf("DEBUG: send %u bytes to connect %s\n", (unsigned int)data_size,
       uuids::to_string(cnt).c_str());
+
+  auto buf = GetBuffer();
+  PacketDataCache pd;
+  pd.PacketData = buf;
+  pd.PacketSize = sizeof(PacketData) + data_size;
+  pd.CtxID = cnt;
+
+  assert((sizeof(PacketData) + data_size) <= kPacketBufferSize);
+  auto pkt = (PacketData*)(pd.PacketData.get());
+  auto cnt_bin = cnt.as_bytes();
+  assert(cnt_bin.size_bytes() == kConnectIDSize);
+  memcpy(pkt->ConnectID, cnt_bin.data(), kConnectIDSize);
+  pkt->PacketCommand = kTrunkCommandDataOut;
+  pkt->PacketIndex = GetPacketIndex(cnt);
+  if (pkt->PacketIndex == kBadPacketIndex) {
+    return false;
+  }
+  pkt->DataSize = data_size;
+
+  std::unique_lock<std::mutex> lk(packet_cache_lock_);
+  packet_data_cache_.push_back(pd);
+  lk.unlock();
+
+  trunk_socket_.async_send_to(boost::asio::buffer(buf.get(), pd.PacketSize),
+      points_.front(),
+      [buf](boost::system::error_code /*ec*/, std::size_t /*bytes_sent*/) {});
+
   return false;
 }
--- common/trunklink.h
+++ common/trunklink.h
@@ -8,6 +8,21 @@
 
 #include "data.h"
 
+
+enum TrunkCommand : uint32_t {
+  kTrunkCommandCreateConnect = 1,
+  kTrunkCommandReleaseConnect = 2,
+  kTrunkCommandAckCreateConnect = 3,
+  kTrunkCommandDataOut = 11,
+  kTrunkCommandDataIn = 12,
+  kTrunkCommandAckDataOut = 21,
+  kTrunkCommandAckDataIn = 22
+};
+
+
+enum TrunkConnectStatus { kTrunkConnectCreated, kTrunkConnectConfirmed };
+
+
 /*! \class TrunkClient Клиентская часть транковой (многоканальной)
 связи */
 class TrunkClient {
@@ -31,6 +46,12 @@ class TrunkClient {
   \param cnt идентификатор коннекта */
   void ReleaseConnect(ConnectID cnt) noexcept;
 
+  /*! Отправить оповещение о новом коннекте
+  \param cnt идентификатор коннекта
+  \param point идентификатор внешней точки кодключения
+  \param timeout таймаут для обмена данными, мс */
+  void SendConnect(ConnectID cnt, PointID point, unsigned int timeout);
+
   /*! Отправить данные по коннекту
   \param cnt идентификатор коннекта
   \param data буфер с данными
@@ -46,19 +67,54 @@ class TrunkClient {
   TrunkClient& operator=(const TrunkClient&) = delete;
   TrunkClient& operator=(TrunkClient&&) = delete;
 
+  static const size_t kPacketBufferSize = 1000;
+  using PacketBuffer = uint8_t[kPacketBufferSize];
+  static const uint32_t kBadPacketIndex = static_cast<uint32_t>(-1);
+
   struct ConnectInfo {
     ConnectID ID;
     std::function<void(ConnectID)> OnDisconnect;
     std::function<void(ConnectID, void*, size_t)> OnData;
+    TrunkConnectStatus Status;
+    uint32_t NextIndex;  //!< Индекс пакета для следующего пакета
   };
 
+  struct PacketConnectCache {
+    ConnectID CtxID;
+    std::shared_ptr<PacketBuffer> PacketData;
+    uint32_t PacketSize;
+  };
+
+
+  struct PacketDataCache {
+    ConnectID CtxID;
+    uint32_t PacketID;
+    std::shared_ptr<PacketBuffer> PacketData;
+    uint32_t PacketSize;
+  };
+
+
   std::vector<boost::asio::ip::udp::endpoint> points_;
+  boost::asio::ip::udp::socket trunk_socket_;
 
   std::vector<ConnectInfo> connects_;
-  std::mutex connect_lock_;
+  std::mutex connects_lock_;
+
+  std::vector<PacketConnectCache> packet_connect_cache_;
+  std::vector<PacketDataCache> packet_data_cache_;
+  std::mutex packet_cache_lock_;
 
 
   std::mt19937 generator_;
+
+  // TODO Descr?
+  void SendConnect(TrunkCommand cmd, ConnectID cnt);
+
+  // TODO Descr?
+  std::shared_ptr<PacketBuffer> GetBuffer();
+
+  // TODO Descr + kBadPacketIndex
+  uint32_t GetPacketIndex(ConnectID cnt);
 };
 
 
