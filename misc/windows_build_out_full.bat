6a7d9fe0e23d1631f40b90e562282c6369bfa6a9
Evgeny Kislov <dev@evgenykislov.com>
2026-01-27 20:43:07 +0300

    Добавлена сборка byte_stacker_out под windows.

---
+++ misc/windows_build_out_full.bat
@@ -0,0 +1,8 @@
+# Compilation
+pushd ..\byte_stacker_out
+
+rd /S /Q build-win-release
+cmake -D CMAKE_MSVC_RUNTIME_LIBRARY=MultiThreaded -B build-win-release
+cmake --build build-win-release --config Release
+
+popd
