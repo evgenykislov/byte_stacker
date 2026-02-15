ab34421136fdd5532c0d26c3b6367d9aeac2e237
Evgeny Kislov <dev@evgenykislov.com>
2026-02-15 12:13:36 +0300

    Добавили генерацию отчёта покрытия кода.

---
+++ make_coverage.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+rm -rfd build_coverage
+
+cmake -DCMAKE_BUILD_TYPE=Debug -DGEN_COVERAGE=ON -B build_coverage
+cmake --build build_coverage
+
+# run tests
+pushd build_coverage/bin
+./tests
+popd
+
+# Generate report
+rm -rfd build_coverage/coverage
+mkdir build_coverage/coverage
+
+gcovr --html build_coverage/coverage/index.html --html-details
