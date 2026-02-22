e3f21d47137f13b350726610fb45423a39c764bc
Evgeny Kislov <dev@evgenykislov.com>
2026-02-22 23:04:03 +0300

    Рефакторинг теста на передачу данных.

--- tests_bash/test_big_data.sh
+++ tests_bash/test_big_data.sh
@@ -1,24 +1,34 @@
 #!/bin/bash
 
 # Тестовый скрипт для проверки передачи большого блока данных
-# В качестве блока данных используется файл с программой byte_stacker_in
+# Тест работает по следующим пунктам:
+# - запускает обе половинки транкового канала через фикстуру
+# - запускает серверную часть, которая готова принимать данные и записывать их в файл
+# - запускает клиентскую часть, которая сразу же начинает передавать данные по каналу
+# - как только клиентская часть завершила передачу, ждём завершения серверной части. Таймаут 5 сек
+# - сравниваются исходный файл для передачи и полученный через канал. Если всё одинаково, то тест прошёл успешно
+#
+# Прим.: большой блок для такого канала - это больше 3 МБайт
 # Параметры (должны быть установлены вызывающим окружением):
 # BIN_PATH - путь к исполняемым файлам
+#
 
 # Название теста
 BTEST_TEST_NAME="Передача большого блока"
 
 
 source ./fixture_direct.sh
+source ./generate_file.sh
 
 
 function test() {
   # Файл с полученными данными
   RECEIVER=-1
-  RES_FILE=$(mktemp)
+  SOURCE_FILE=$(generatefile 5000000)
+  DESTINATION_FILE=$(mktemp)
   RES=0
   
-  nc -4 -l 127.0.0.2 50001 > ${RES_FILE} < /dev/null &
+  nc -4 -l 127.0.0.2 50001 > ${DESTINATION_FILE} < /dev/null &
   RECEIVER=$!
   if ! ps -p ${RECEIVER} > /dev/null 2>&1 ; then
     echo "receiver hasn't run"
@@ -26,20 +36,20 @@ function test() {
   fi
 
   if [[ ${RES} == 0 ]]; then
-    nc -4 -N 127.0.0.2 30001 < "${BIN_PATH}/byte_stacker_in"
+    nc -4 -N 127.0.0.2 30001 < ${SOURCE_FILE}
     timeout 5 tail --pid=${RECEIVER} -f /dev/null
 
     if ps -p ${RECEIVER} > /dev/null 2>&1 ; then
       echo "receiver hasn't stopped after receiving data"
 	  RES=1
     else
-      echo "Transferred $(stat -c %s "${RES_FILE}") bytes"
+      echo "Transferred $(stat -c %s "${DESTINATION_FILE}") bytes"
       RECEIVER=-1
 	fi
   fi
 
   if [[ ${RES} == 0 ]]; then
-    if ! cmp -s "${BIN_PATH}/byte_stacker_in" ${RES_FILE} >/dev/null ; then
+    if ! cmp -s "${SOURCE_FILE}" ${DESTINATION_FILE} >/dev/null ; then
       echo "Files are different"
 	  RES=1
     fi
@@ -49,8 +59,11 @@ function test() {
   if [[ ${RECEIVER} != -1 ]]; then
     kill ${RECEIVER}
   fi
-  if [[ ${RES_FILE} != "" ]]; then
-    rm -f ${RES_FILE}
+  if [[ ${DESTINATION_FILE} != "" ]]; then
+    rm -f ${DESTINATION_FILE}
+  fi
+  if [[ ${SOURCE_FILE} != "" ]]; then
+    rm -f ${SOURCE_FILE}
   fi
   
   return ${RES}
