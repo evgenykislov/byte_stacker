0b767a0189435395aba57efd1fbb48215cc63409
Evgeny Kislov <dev@evgenykislov.com>
2026-07-19 16:25:08 +0300

    Доработка интерфейса.

--- clients/ios/stacker/stacker/ViewController.swift
+++ clients/ios/stacker/stacker/ViewController.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var run_btn_: UIButton!
+    @IBOutlet weak var stop_btn_: UIButton!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func OnRun(_ sender: UIButton) {
+    }
+    
+    @IBAction func OnStop(_ sender: UIButton) {
+    }
 }
 
