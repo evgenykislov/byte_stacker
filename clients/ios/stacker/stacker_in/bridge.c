e8f8ae0b3cd8662c8e4059be5e42345b92a841a6
Evgeny Kislov <dev@evgenykislov.com>
2026-08-01 21:56:28 +0300

    Добавили бридж в С++ код для extension.

---
+++ clients/ios/stacker/stacker_in/bridge.c
@@ -0,0 +1,8 @@
+//
+//  bridge.c
+//  stacker_in
+//
+//  Created by Евгений on 01.08.2026.
+//
+
+#include "bridge.h"
---
+++ clients/ios/stacker/stacker_in/bridge.h
@@ -0,0 +1,13 @@
+//
+//  bridge.h
+//  stacker_in
+//
+//  Created by Евгений on 01.08.2026.
+//
+
+#ifndef bridge_h
+#define bridge_h
+
+#include <stdio.h>
+
+#endif /* bridge_h */
