2c1e131ed615703454348d0ce81a55e8096e658f
Evgeny Kislov <dev@evgenykislov.com>
2026-08-13 21:29:54 +0300

    Добавлена обработка пропуска пакетов с командой на соединение.

--- utils/udp_processor/processor_skip.h
+++ utils/udp_processor/processor_skip.h
@@ -39,4 +39,39 @@ class ProcessorSkip: public Processor {
   unsigned long counter_;
 };
 
+
+class ProcessorSkipConnection: public Processor {
+ public:
+  ProcessorSkipConnection(std::shared_ptr<Processor> next, std::string value)
+      : Processor(next) {
+    try {
+      n_ = std::stoul(value);
+    } catch (std::exception&) {
+      std::cerr << "Skip parameter error. Disable skipping" << std::endl;
+      n_ = 0;
+    }
+    counter_ = 0;
+  }
+
+  /*! Отправить пакет в обработку */
+  virtual void Push(PacketInfoPtr packet) {
+    if (packet->size_ >= 20) {
+      if (*(uint32_t*)&packet->data_[16] == 1) {
+        // Это команда на установление соединения
+        if (counter_ < n_) {
+          ++counter_;
+          return; // Пропускаем пакет
+        }
+      }
+    }
+
+    next_processor_->Push(packet);
+  }
+
+ private:
+  unsigned long n_;
+  unsigned long counter_;
+};
+
+
 #endif  // PROCESSOR_SKIP_H
