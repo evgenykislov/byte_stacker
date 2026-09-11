6a9e01d32b0d972dd20d607f6e8c1b8503b23c25
Evgeny Kislov <dev@evgenykislov.com>
2026-09-12 02:42:38 +0300

    Добавлена заготовка приложения.

---
+++ byte_stacker_ios/stacker/stacker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  stacker
+//
+//  Created by Евгений on 12.09.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
